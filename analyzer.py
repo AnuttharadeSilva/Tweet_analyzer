306725bec42e972f21d99e51e8cac70077ba67aa
AnuttharadeSilva <desilvaanu96@gmail.com>
2020-06-09 01:50:05 +0530

    new models added

--- analyzer.py
+++ analyzer.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import re
 import nltk
-from nltk.stem.porter import PorterStemmer
+import spacy
 from string import punctuation 
 from sklearn.externals import joblib
 from sklearn.naive_bayes import GaussianNB
@@ -10,47 +10,44 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 
 def predict(tweets):
-    model1 = open('model/logisticR.pkl','rb')
-    #model2 = open('model/SVM.pkl','rb')
-    #model3 = open('model/kernelSVM.pkl','rb')
-    model4 = open('model/randomF.pkl','rb')
-    model5 = open('model/dTree.pkl','rb')
+    model1 = open('model/lr_new.pkl','rb')
+    model2 = open('model/rf_new.pkl','rb')
+    model3 = open('model/dt_new.pkl','rb')
     clf1 = joblib.load(model1)
-    #clf2 = joblib.load(model2)
-    #clf3 = joblib.load(model3)
-    clf4 = joblib.load(model4)
-    clf5 = joblib.load(model5)
+    clf2 = joblib.load(model2)
+    clf3 = joblib.load(model3)
     
     from nltk.corpus import stopwords
     nltk.download('stopwords')
+    spc = spacy.load('en')
     stopwords = set(stopwords.words('english')+list(punctuation)+['AT_USER','URL'])
-    corpus=[]
-    for tweet in tweets:
+
+    corpus = []
+    for tweet in tweets:    
         tweet = re.sub('[^a-zA-Z]', ' ', tweet)
         tweet = tweet.lower()
-        tweet = tweet.split()
-        ps = PorterStemmer()
-        tweet = [ps.stem(word) for word in tweet if not word in stopwords ]
+        tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet)
+        tweet = re.sub('@[^\s]+', 'AT_USER', tweet)
+        doc = spc(tweet)
+        tweet = [word.lemma_ for word in doc if not word in stopwords ]
         tweet = ' '.join(tweet)
         corpus.append(tweet)
 
     #data = [tweet]
-    cv_model = open('model/countV.pkl','rb')
-    td_model = open('model/tfidf.pkl','rb')
+    cv_model = open('model/cv_new.pkl','rb')
+    td_model = open('model/td_new.pkl','rb')
     cv = joblib.load(cv_model)
     td = joblib.load(td_model)
 		
     vect = cv.transform(corpus).toarray()
     vect = td.transform(vect).toarray()
     pred1= clf1.predict(vect)
-    #pred2= clf2.predict(vect)
-    #pred3= clf3.predict(vect)
-    pred4= clf4.predict(vect)
-    pred5= clf5.predict(vect)
+    pred2= clf2.predict(vect)
+    pred3= clf3.predict(vect)
 
     pred =[]
     for k in range(0,len(pred1)):
-        a=[pred1[k],pred1[k],pred1[k],pred4[k],pred5[k]]
+        a=[pred1[k],pred1[k],pred1[k],pred2[k],pred3[k]]
         if a.count(0)> a.count(1):
             pred.append(0)
         else:
