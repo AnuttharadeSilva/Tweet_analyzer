3b646c5023dd6e1f9552e6020d42ca706664c55e
AnuttharadeSilva <desilvaanu96@gmail.com>
2020-06-29 12:19:22 +0530

    change

--- app.py
+++ app.py
@@ -17,34 +17,34 @@ Bootstrap(app)
 @cross_origin()
 def index():
     if request.method == 'POST':
-        try:
-            key1 = request.form['key1']
-            key2 = request.form['key2']
-            limit = int(request.form['limit'])
-            begin = request.form['begindate']
-            end= request.form['enddate']
+    #try:
+        key1 = request.form['key1']
+        key2 = request.form['key2']
+        limit = int(request.form['limit'])
+        begin = request.form['begindate']
+        end= request.form['enddate']
 
-            keys = " AND ".join([key1,key2])
-            begin_date = date(*map(int, begin.split('-')))
-            end_date = date(*map(int, end.split('-')))
+        keys = " AND ".join([key1,key2])
+        begin_date = date(*map(int, begin.split('-')))
+        end_date = date(*map(int, end.split('-')))
 
-            #start_time = timeit.default_timer()
-            #tweets = twitter_scraper.stream(keys,begin_date,end_date,limit)
-            tweets = tweepy_streamer.get_tweets(keys,begin_date,end_date,limit)
-            #print("time: ",timeit.default_timer() - start_time)
+        #start_time = timeit.default_timer()
+        #tweets = twitter_scraper.stream(keys,begin_date,end_date,limit)
+        tweets = tweepy_streamer.get_tweets(keys,begin_date,end_date,limit)
+        #print("time: ",timeit.default_timer() - start_time)
 
-            if len(tweets)>0:
-                result = analyzer.predict(tweets)
-                #result = result.tolist()
-                percent = [(result.count(0))/len(result)*100 , (result.count(1))/len(result)*100]
+        if len(tweets)>0:
+            result = analyzer.predict(tweets)
+            #result = result.tolist()
+            percent = [(result.count(0))/len(result)*100 , (result.count(1))/len(result)*100]
 
-                return jsonify({'percent' : percent, 'tweets' : tweets, 'predictions' : result})
-                #return render_template('show.html',result=result,tweets=tweets,number=len(tweets),percent=percent)
-            else:
-                return jsonify({'percent' : None, 'tweets' : None, 'predictions' : None})
+            return jsonify({'percent' : percent, 'tweets' : tweets, 'predictions' : result})
+            #return render_template('show.html',result=result,tweets=tweets,number=len(tweets),percent=percent)
+        else:
+            return jsonify({'percent' : None, 'tweets' : None, 'predictions' : None})
 
-        except:
-            return jsonify({'message' : 'Input values are wrong.'})
+    #except:
+        #return jsonify({'message' : 'Input values are wrong.'})
             
 
     return render_template('index.html')
