57a603d69b450036743915c21fe05f3b12a2484d
AnuttharadeSilva <desilvaanu96@gmail.com>
2020-09-01 07:09:13 +0530

    test files updated

--- analyzer.py
+++ analyzer.py
@@ -1,63 +1,69 @@
+import numpy as np
 import pandas as pd 
 import re
 import nltk
-import spacy
+#import spacy
 from string import punctuation 
+from nltk.stem.porter import PorterStemmer
 #from sklearn.externals import joblib
 import joblib
-from sklearn.naive_bayes import GaussianNB
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 
 
 def predict(tweets):
-#try:
-    model1 = open('model/lr_new.pkl','rb')
-    model2 = open('model/rf_new.pkl','rb')
-    model3 = open('model/dt_new.pkl','rb')
-    clf1 = joblib.load(model1)
-    clf2 = joblib.load(model2)
-    clf3 = joblib.load(model3)
-    
-    from nltk.corpus import stopwords
-    nltk.download('stopwords')
-    spc = spacy.load('en')
-    stopwords = set(stopwords.words('english')+list(punctuation)+['AT_USER','URL'])
-
-    corpus = []
-    for tweet in tweets:    
-        tweet = re.sub('[^a-zA-Z]', ' ', tweet)
-        tweet = tweet.lower()
-        tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet)
-        tweet = re.sub('@[^\s]+', 'AT_USER', tweet)
-        doc = spc(tweet)
-        tweet = [word.lemma_ for word in doc if not word in stopwords ]
-        tweet = ' '.join(tweet)
-        corpus.append(tweet)
-
-    #data = [tweet]
-    cv_model = open('model/cv_new.pkl','rb')
-    td_model = open('model/td_new.pkl','rb')
-    cv = joblib.load(cv_model)
-    td = joblib.load(td_model)
+    try:
+        model1 = open('model/logistic_regression.pkl','rb')
+        model2 = open('model/random_forest.pkl','rb')
+        model3 = open('model/decision_tree.pkl','rb')
+        clf1 = joblib.load(model1)
+        clf2 = joblib.load(model2)
+        clf3 = joblib.load(model3)
         
-    vect = cv.transform(corpus).toarray()
-    vect = td.transform(vect).toarray()
-    pred1= clf1.predict(vect)
-    pred2= clf2.predict(vect)
-    pred3= clf3.predict(vect)
+        from nltk.corpus import stopwords
+        nltk.download('stopwords')
+        nltk.download('wordnet')
+        #spc = spacy.load('en')
+        stopwords = set(stopwords.words('english')+list(punctuation)+['AT_USER','URL'])
 
-    pred =[]
-    for k in range(0,len(pred1)):
-        a=[pred1[k],pred1[k],pred1[k],pred2[k],pred3[k]]
-        if a.count(0)> a.count(1):
-            pred.append(0)
-        else:
-            pred.append(1)
+        corpus = []
+        for tweet in tweets:    
+            tweet = re.sub('[^a-zA-Z]', ' ', tweet)
+            tweet = tweet.lower()
+            tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet)
+            tweet = re.sub('@[^\s]+', 'AT_USER', tweet)
+            #tweet = tweet.split()
+            tokenizer = nltk.tokenize.TreebankWordTokenizer()
+            tweet = tokenizer.tokenize(tweet)
+            #ps = PorterStemmer()
+            lmz = nltk.stem.WordNetLemmatizer()
+            tweet = [lmz.lemmatize(word) for word in tweet if not word in stopwords ]
+            tweet = ' '.join(tweet)
+            corpus.append(tweet)
 
-    return pred
+        #data = [tweet]
+        cv_model = open('model/count_vectorizer.pkl','rb')
+        td_model = open('model/tfidf_vectorizer.pkl','rb')
+        cv = joblib.load(cv_model)
+        td = joblib.load(td_model)
+            
+        vect = cv.transform(corpus).toarray()
+        vect = td.transform(vect).toarray()
+        pred1= clf1.predict(vect)
+        pred2= clf2.predict(vect)
+        pred3= clf3.predict(vect)
 
-#except:
-    #return []
+        pred =[]
+        for k in range(0,len(pred1)):
+            a=[pred1[k],pred1[k],pred1[k],pred2[k],pred3[k]]
+            if a.count(0)> a.count(1):
+                pred.append(0)
+            else:
+                pred.append(1)
+
+        return pred
+
+    except:
+        return []
 
 
--- locustfile.py
+++ locustfile.py
@@ -1,17 +1,18 @@
-from locust import HttpUser, task, between
+# from locust import HttpUser, task, between
 
-class WebsiteTestUser(HttpUser):
-    wait_time = between(0.5, 3.0)
+# class WebsiteTestUser(HttpUser):
+#     wait_time = between(0.5, 3.0)
     
-    def on_start(self):
-        """ on_start is called when a Locust start before any task is scheduled """
-        pass
+#     def on_start(self):
+#         pass
 
-    def on_stop(self):
-        """ on_stop is called when the TaskSet is stopping """
-        pass
+#     def on_stop(self):
+#         pass
 
-    @task(1)
-    def index(self):
-        #self.client.get("http://0.0.0.0:5002")
-        self.client.post("http://0.0.0.0:5002", {"key1":"covid", "key2":"usa", "begindate":"2020-6-1", "enddate":"2020-6-20", "limit":"10"})
+#     @task(1)
+#     def get_request(self):
+#         self.client.get("http://0.0.0.0:5002")
+
+#     @task(2)
+#     def post_request(self):
+#         self.client.post("http://0.0.0.0:5002", {"key1":"covid", "key2":"usa", "begindate":"2020-6-1", "enddate":"2020-6-20", "limit":"10"})
